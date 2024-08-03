d2599ca975565ec52676b35c02c4e3bf3e21141e
LisaMandarin <chu23001@byui.edu>
2024-08-04 04:25:20 +0800

    appwrite

---
+++ src/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66abbd80001d74153b78')
+
+export const account = new Account(client);
+export const databases = new Databases(client); 
