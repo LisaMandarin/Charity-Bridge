cca2dd5ed114ece2d073fa81580235d49d50735e
LisaMandarin <chu23001@byui.edu>
2024-09-01 19:26:05 +0800

    userForm fix

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -4,6 +4,7 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { useUser } from "../lib/context/user";
 import { useUserInfos } from "../lib/context/userInfo";
 import { UserList } from "./UserList";
+import '../output.css'
 
 dayjs.extend(customParseFormat);
 const { Title } = Typography
@@ -55,72 +56,72 @@ export function UserForm() {
     }
 
     return (
-        <>
-        <Form
-            labelCol={{span: 6}}
-            wrapperCol={{span: 14}}
-            className="w-96 bg-white m-auto p-4"
-            form={form}
-            name="user-form"
-            onFinish={(values) => userInfo.add({
-                userID: user.current.$id, ...values
-            })}
-            onFinishFailed={(errorInfo) => console.log('Failed: ', errorInfo)}
-        >
-            <Title underline className="text-center">User Form</Title>
-            <Form.Item
-                label="name"
-                name="name"
-                rules={[
-                    {required: true, message: "Please enter Name"}
-                ]}
+        <div>
+            <Form
+                labelCol={{span: 6}}
+                wrapperCol={{span: 14}}
+                className="w-96 bg-white m-auto p-4"
+                form={form}
+                name="user-form"
+                onFinish={(values) => userInfo.add({
+                    userID: user.current.$id, ...values
+                })}
+                onFinishFailed={(errorInfo) => console.log('Failed: ', errorInfo)}
             >
-                <Input placeholder="Please enter name"/>
-            </Form.Item>
-            <Form.Item
-                label="birthday"
-                name="birthday"
-                rules={[
-                    { required: true, message: 'Please select birthday' },
-                ]}
-            >
-                <DatePicker
-                    maxDate={dayjs(getToday(), dateFormat)}
-                    onChange={onBirthdayChange}
-                />
-            </Form.Item>
-            <Form.Item
-                label="age"
-                name="age"
-            >
-                <InputNumber disabled />
-            </Form.Item>
-            <Form.Item
-                label="native"
-                name="native"
-                rules={[
-                    {required: true, message: "Please choose native or non-native"}
-                ]}
-            >
-                <Radio.Group>
-                    <Radio value={true}>Yes</Radio>
-                    <Radio value={false}>No</Radio>
-                </Radio.Group>
-            </Form.Item>
-            <Form.Item
-                label="address"
-                name="address"
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                wrapperCol={{offset: 6}}
-            >
-                <Button type="primary" htmlType="submit">Submit</Button>
-                <Button onClick={onReset}>Reset</Button>
-            </Form.Item>
-        </Form>
-        <UserList />
-        </>
+                <Title underline className="text-center">User Form</Title>
+                <Form.Item
+                    label="name"
+                    name="name"
+                    rules={[
+                        {required: true, message: "Please enter Name"}
+                    ]}
+                >
+                    <Input placeholder="Please enter name"/>
+                </Form.Item>
+                <Form.Item
+                    label="birthday"
+                    name="birthday"
+                    rules={[
+                        { required: true, message: 'Please select birthday' },
+                    ]}
+                >
+                    <DatePicker
+                        maxDate={dayjs(getToday(), dateFormat)}
+                        onChange={onBirthdayChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="age"
+                    name="age"
+                >
+                    <InputNumber disabled />
+                </Form.Item>
+                <Form.Item
+                    label="native"
+                    name="native"
+                    rules={[
+                        {required: true, message: "Please choose native or non-native"}
+                    ]}
+                >
+                    <Radio.Group>
+                        <Radio value={true}>Yes</Radio>
+                        <Radio value={false}>No</Radio>
+                    </Radio.Group>
+                </Form.Item>
+                <Form.Item
+                    label="address"
+                    name="address"
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    wrapperCol={{offset: 6}}
+                >
+                    <Button type="primary" htmlType="submit">Submit</Button>
+                    <Button onClick={onReset}>Reset</Button>
+                </Form.Item>
+            </Form>
+            <UserList />
+        </div>
     )
 }
