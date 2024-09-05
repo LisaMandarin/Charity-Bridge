a60c03e9cc10536c42aea8cce9fa901f9b2ad107
LisaMandarin <chu23001@byui.edu>
2024-09-06 03:25:41 +0800

    fix path

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,7 +2,7 @@ import { Button, Table, Input, Space } from "antd";
 import { useUserInfos } from "../lib/context/userInfo";
 import { useEffect, useState } from "react";
 import { nativeYN, bDayFormat, fetchInfos, onSearch } from "./userListUtil";
-import { UserModal } from "./UserModal";
+import { UserModal } from "./userModal";
 const { Search } = Input
 
 export function UserList() {
