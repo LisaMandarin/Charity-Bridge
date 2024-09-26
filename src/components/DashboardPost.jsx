58e76f65c4959c80fc69ca54e3b9dc03a8e16b92
LisaMandarin <chu23001@byui.edu>
2024-09-27 02:14:18 +0800

    add require

--- src/components/DashboardPost.jsx
+++ src/components/DashboardPost.jsx
@@ -3,18 +3,18 @@ import Upload from "antd/es/upload/Upload"
 
 export function DashboardPost() {
     const [ form ] = Form.useForm()
-    const onFinish = () => {
-
+    const onFinish = (values) => {
+        console.log('form values: ', values)
     }
-    const onFinishFailed = () => {
-
+    const onFinishFailed = (errorInfo) => {
+        console.log('form errorInfo: ', errorInfo)
     }
     const onReset = () => {
-
+        form.resetFields()
     }
     
     return (
-        <>
+        <div className="md:w-[600px]">
             <Form
                 name="post-form"
                 form={form}
@@ -30,6 +30,12 @@ export function DashboardPost() {
                 <Form.Item
                     label="Product"
                     name="product"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter product name'
+                        }
+                    ]}
                 >
                     <Input />
                 </Form.Item>
@@ -48,18 +54,36 @@ export function DashboardPost() {
                 <Form.Item
                     label="Category"
                     name="category"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please choose a category'
+                        }
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Description"
                     name="description"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter product description'
+                        }
+                    ]}
                 >
                     <Input.TextArea />
                 </Form.Item>
                 <Form.Item
                     label="Photos"
                     name="photos"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please upload at least one photo of the product'
+                        }
+                    ]}
                 >
                     <Upload
                         listType="picture-card"
@@ -73,11 +97,11 @@ export function DashboardPost() {
                     }}
                 >
                     <Space>
-                        <Button type="primary">Post</Button>
+                        <Button type="primary" htmlType="submit">Post</Button>
                         <Button onClick={onReset}>Reset</Button>
                     </Space>
                 </Form.Item>
             </Form>
-        </>
+        </div>
     )
 }
