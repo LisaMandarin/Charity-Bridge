f53dbe7cec079d78543c345549155f0298f77131
LisaMandarin <chu23001@byui.edu>
2024-11-22 21:15:49 +0800

    Fix nav style

--- src/components/Header/HeaderNavH.jsx
+++ src/components/Header/HeaderNavH.jsx
@@ -5,16 +5,14 @@ import { Link } from 'react-router-dom';
 export function HeaderNavH({items}) {
         
     return (
-        <>
-            <div className='grid grid-cols-3 h-full gap-y-2 md:gap-x-12'>
-                { items.map(item => (
-                    <Space key={item.key}>
-                        <Icon icon={item.icon}/>
-                        <Link to={item.path}>{item.text}</Link>
-                    </Space>
-                ))}
-            </div>
-        </>
+        <div className='grid grid-cols-3 h-full gap-y-2 md:gap-x-12'>
+            { items.map(item => (
+                <Space key={item.key}>
+                    <Icon icon={item.icon}/>
+                    <Link to={item.path}>{item.text}</Link>
+                </Space>
+            ))}
+        </div>
     )
 
     
--- src/components/Header/HeaderNavV.jsx
+++ src/components/Header/HeaderNavV.jsx
@@ -11,36 +11,33 @@ export function HeaderNavV ({items}) {
 
     return (
         <>
-        <div className='flex h-full justify-center items-center'>
-            <FaBars 
-                onClick={toggleShow}
-                className='cursor-pointer w-8 h-8'
-            />
-        </div>
-        <ul
-            className='bg-pink-200 z-40 flex flex-col fixed top-24 left-0 w-60 overflow-hidden'
-            style={{
-                transform: show ? 'translateX(0)' : 'translateX(-100%)',
-                transition: 'transform .5s ease-in-out',
-                
-            }}
-        >
-            {items.map(item => (
-                <li key={item.key} className='w-full px-8 py-3'>
-                        <Link to={item.path} onClick={toggleShow}>
-                            <Icon icon={item.icon} className='inline'/>
-                            <span> {item.text}</span>
-                        </Link>
-                </li>
-            ))}
-            <FaTimes
-                onClick={toggleShow}
-                className='w-8 h-8 cursor-pointer mx-auto'
-            />
-        </ul>
-        
-        
-        
+            <div className='flex h-full justify-center items-center'>
+                <FaBars 
+                    onClick={toggleShow}
+                    className='cursor-pointer w-10 h-10'
+                />
+            </div>
+            <ul
+                className='bg-pink-200 z-40 flex flex-col fixed top-24 left-0 w-full overflow-hidden'
+                style={{
+                    transform: show ? 'translateX(0)' : 'translateX(-100%)',
+                    transition: 'transform .5s ease-in-out',
+                    
+                }}
+            >
+                {items.map(item => (
+                    <li key={item.key} className='w-full px-8 py-3 text-center'>
+                            <Link to={item.path} onClick={toggleShow}>
+                                <Icon icon={item.icon} className='inline'/>
+                                <span> {item.text}</span>
+                            </Link>
+                    </li>
+                ))}
+                <FaTimes
+                    onClick={toggleShow}
+                    className='w-10 h-10 cursor-pointer mx-auto'
+                />
+            </ul>
         </>
     )
 }
