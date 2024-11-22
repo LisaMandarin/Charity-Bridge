1a99fc4473ca7a1f85c072d652df90b2cfaf3787
LisaMandarin <chu23001@byui.edu>
2024-11-22 21:53:02 +0800

    Fix Home order

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,19 @@
-import { useMediaQuery } from "react-responsive"
 import { HomePost } from "./HomePost"
 import { HomeReview } from "./HomeReview"
 import { HomeNeed } from "./HomeNeed"
 
 export function Home() {
-    const isMobile = useMediaQuery({maxWidth: 640})
-
     return (
         <div className="grid sm:grid-cols-[1fr_3fr_1fr] sm:mx-4 lg:mx-8">
-            { isMobile ? (
-                <>
-                    <HomePost />
-                    <HomeReview />
-                    <HomeNeed />
-                </>
-            ) : (
-                <>
-                    <HomeReview />
-                    <HomePost />
-                    <HomeNeed />
-                </>
-            )}
+            <div className="order-2 sm:order-1">
+                <HomeReview />
+            </div>
+            <div className="order-1 sm:order-2">
+                <HomePost />
+            </div>
+            <div className="order-3">
+                <HomeNeed />      
+            </div>
         </div>
     )
 }
