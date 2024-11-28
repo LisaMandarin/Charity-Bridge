fac5efb71b617e43f503f70737f4c9eb13ce102a
LisaMandarin <chu23001@byui.edu>
2024-11-28 13:55:30 +0800

    add loading to product by category

--- src/components/Product/ProductByCategory.jsx
+++ src/components/Product/ProductByCategory.jsx
@@ -1,7 +1,7 @@
 import { Query } from "appwrite";
 import { useEffect, useState } from "react";
 import { useProductInfo } from "../../lib/context/productInfo";
-import { Card, message, Typography } from "antd";
+import { Card, message, Spin, Typography } from "antd";
 import { Link, useParams } from "react-router-dom";
 const { Title } = Typography;
 const { Meta } = Card;
@@ -27,33 +27,35 @@ export function ProductByCategory() {
 
   return (
     <div className="p-8 text-center">
-      <Title>{category}</Title>
-      <div className="flex flex-wrap bg-white gap-4 justify-center">
-        {documents &&
-          documents.map((d) => (
-            <Link to={`/product/${d.$id}`} key={d.$id}>
-              <Card
-                className="w-full md:w-[360px]"
-                hoverable
-                cover={
-                  <div className="w-full aspect-square">
-                    <img
-                      alt={`{d.product}'s photo`}
-                      src={d.photoURL[0]}
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain",
-                      }}
-                    />
-                  </div>
-                }
-              >
-                <Meta title={<div className="text-wrap">{d.product}</div>} />
-              </Card>
-            </Link>
-          ))}
-      </div>
+      <Spin spinning={productInfo.loading}>
+        <Title>{category.toUpperCase()}</Title>
+        <div className="flex flex-wrap bg-white gap-4 justify-center">
+          {documents &&
+            documents.map((d) => (
+              <Link to={`/product/${d.$id}`} key={d.$id}>
+                <Card
+                  className="w-full md:w-[360px]"
+                  hoverable
+                  cover={
+                    <div className="w-full aspect-square">
+                      <img
+                        alt={`{d.product}'s photo`}
+                        src={d.photoURL[0]}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </div>
+                  }
+                >
+                  <Meta title={<div className="text-wrap">{d.product}</div>} />
+                </Card>
+              </Link>
+            ))}
+        </div>
+      </Spin>
     </div>
   );
 }
