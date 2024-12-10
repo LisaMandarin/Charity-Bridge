2940fc05d2f107cdc3c60bd30e0456c380e21c7d
LisaMandarin <chu23001@byui.edu>
2024-12-11 04:40:18 +0800

    fix: badge position in HeaderUser

--- src/components/Header/HeaderUser.jsx
+++ src/components/Header/HeaderUser.jsx
@@ -1,44 +1,41 @@
 import { Flex, Button, Avatar, Tooltip, Badge } from "antd";
 import { useUser } from "../../lib/context/user";
-import { UserOutlined } from "@ant-design/icons";
+import { UserOutlined, WarningFilled } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import { Icon } from "@iconify/react/dist/iconify.js";
+import Icon from "@ant-design/icons";
+// import { Icon } from "@iconify/react/dist/iconify.js";
 import { useEffect, useState } from "react";
 
 export function HeaderUser() {
   const user = useUser();
   const [profileId, setProfileId] = useState(null);
-  const [openToolTip, setOpenToolTip] = useState("");
 
   useEffect(() => {
     if (user?.current?.prefs?.profileId) {
       setProfileId(user.current.prefs.profileId);
+    } else {
+      setProfileId(null);
     }
   }, [user?.current?.prefs?.profileId]);
-
-  useEffect(() => {
-    if (profileId === null) {
-      setOpenToolTip(
-        <Tooltip title="You haven't created your profile" color="gold">
-          <Icon
-            icon="noto:warning"
-            width="1rem"
-            height="1rem"
-            className="inline"
-          />
-        </Tooltip>,
-      );
-    } else {
-      setOpenToolTip(null);
-    }
-  }, [profileId]);
 
   return (
     <>
       {user?.current ? (
         <div className="w-fit flex flex-row justify-around items-end gap-4 p-1 h-24 border-4 border-pink-200">
           <Link to="/dashboard">
-            <Badge count={openToolTip} className="flex items-end h-fit">
+            <Badge
+              count={
+                profileId === null ? (
+                  <div>
+                    <Tooltip title="You haven't created a profile">
+                      <WarningFilled style={{ fontSize: "1.5rem" }} />
+                    </Tooltip>
+                  </div>
+                ) : null
+              }
+              offset={[0, 10]}
+              className="flex items-end h-fit"
+            >
               <Avatar
                 size={60}
                 icon={<UserOutlined />}
