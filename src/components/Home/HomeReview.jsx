fdd7c05c6a6617f5836422a4fc98405a83ccaea3
LisaMandarin <chu23001@byui.edu>
2024-12-18 02:52:59 +0800

    fix: cut review content short

--- src/components/Home/HomeReview.jsx
+++ src/components/Home/HomeReview.jsx
@@ -1,8 +1,7 @@
-import { StarTwoTone } from "@ant-design/icons";
 import { useReviews } from "../../lib/context/reviews";
 import { getUser } from "../../lib/serverAppwrite";
 import { useProductInfo } from "../../lib/context/productInfo";
-import { Avatar, Pagination, Space, Typography } from "antd";
+import { Avatar, Pagination, Rate, Space, Spin, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 const { Title } = Typography;
@@ -14,15 +13,18 @@ export function HomeReview() {
   const [donorList, setDonorList] = useState([]);
   const [receiverList, setReceiverList] = useState([]);
   const [productList, setProductList] = useState([]);
+  const [expandedItems, setExpandedItems] = useState({});
+
+  const toggleContent = (i) =>
+    setExpandedItems((current) => ({ ...current, [i]: !current[i] }));
+
+  /* ***********************
+     Handle items per page
+  ************************ */
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(3);
   const startIndex = (currentPage - 1) * itemsPerPage;
   const currentItems = reviewList.slice(startIndex, startIndex + itemsPerPage);
-  const showStars = (starsNumber) => {
-    return Array.from({ length: starsNumber }, (_, index) => (
-      <StarTwoTone key={index} twoToneColor="#eb2f96" />
-    ));
-  };
 
   /* ******************************
   Fetch reviews in initial render
@@ -76,53 +78,84 @@ export function HomeReview() {
     fetchNames();
   }, [currentItems]);
 
+  useEffect(() => {
+    console.log("expandedItems: ", expandedItems);
+  }, [expandedItems]);
+
   return (
     <div className="flex flex-col justify-between h-full px-4">
       <Title level={2} className="text-center pt-4">
         Words of Thanks
       </Title>
       <Space size="large" direction="vertical" className="text-xs flex-grow">
-        {donorList.length > 0 &&
-          receiverList.length > 0 &&
-          productList.length > 0 &&
-          currentItems.map((review, i) => (
-            <div key={i}>
-              <p>
-                <Avatar icon="U" />{" "}
-                <span>
+        <Spin spinning={reviews.loading}>
+          {donorList.length > 0 &&
+            receiverList.length > 0 &&
+            productList.length > 0 &&
+            currentItems.map((review, i) => (
+              <div key={i}>
+                <p>
+                  <Avatar icon="U" />{" "}
+                  <span>
+                    <Link
+                      to={`/userProduct/${review.receiverId}`}
+                      className="text-blue-500"
+                    >
+                      {receiverList[i].name}
+                    </Link>{" "}
+                    said
+                  </span>
+                  ,
+                </p>
+                <p className="indent-8">
+                  Thank you,{" "}
                   <Link
-                    to={`/userProduct/${review.receiverId}`}
+                    to={`/userProduct/${review.donorId}`}
                     className="text-blue-500"
                   >
-                    {receiverList[i].name}
-                  </Link>{" "}
-                  said
-                </span>
-                ,
-              </p>
-              <p className="indent-8">
-                Thank you,{" "}
-                <Link
-                  to={`/userProduct/${review.donorId}`}
-                  className="text-blue-500"
-                >
-                  {donorList[i].name}
-                </Link>
-                ,
-              </p>
-              <p>
-                I give you {showStars(review.stars)} for the donation of the{" "}
-                <Link
-                  to={`/product/${review.productId}`}
-                  className="text-blue-500"
-                >
-                  {productList[i].product.toLocaleLowerCase()}
-                </Link>
-                .
-              </p>
-              <p>{review.reviewContent}</p>
-            </div>
-          ))}
+                    {donorList[i].name}
+                  </Link>
+                  ,
+                </p>
+                <p>
+                  I give you{" "}
+                  <Rate
+                    count={5}
+                    defaultValue={review.stars}
+                    disabled
+                    className="text-sm"
+                  />{" "}
+                  for the donation of the{" "}
+                  <Link
+                    to={`/product/${review.productId}`}
+                    className="text-blue-500"
+                  >
+                    {productList[i].product.toLocaleLowerCase()}
+                  </Link>
+                  .
+                </p>
+                <p className={`${!expandedItems[i] ? "truncate" : ""}`}>
+                  {review.reviewContent}
+                </p>
+                {!expandedItems[i] && (
+                  <span
+                    className="text-blue-500 cursor-pointer"
+                    onClick={() => toggleContent(i)}
+                  >
+                    ...more
+                  </span>
+                )}
+                {expandedItems[i] && (
+                  <span
+                    className="text-blue-500 cursor-pointer"
+                    onClick={() => toggleContent(i)}
+                  >
+                    Show less
+                  </span>
+                )}
+              </div>
+            ))}
+        </Spin>
       </Space>
       <Pagination
         simple={{ readOnly: true }}
