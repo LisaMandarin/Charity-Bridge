a2f8c6a8a94220da56cb76389822ecd2f399e943
LisaMandarin <chu23001@byui.edu>
2024-12-22 03:43:33 +0800

    fix: welcome slide back animation

--- src/components/Dashboard/DashboardMessageList.jsx
+++ src/components/Dashboard/DashboardMessageList.jsx
@@ -1,6 +1,10 @@
 import { Avatar } from "antd";
+import { useEffect } from "react";
+import { useMessage } from "../../lib/context/messages";
 
-export function MessageList() {
+export function MessageList({ user }) {
+  const message = useMessage();
+
   return (
     <div className="flex flex-row gap-2 p-4">
       <div className="self-center">
