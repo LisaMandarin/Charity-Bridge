736f2b7a8354d78d60acd16183e7f1d4b49196d9
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-16 13:06:59 +0800

    fix: delete unnecessary import

--- src/components/Dashboard/DashboardMessage.jsx
+++ src/components/Dashboard/DashboardMessage.jsx
@@ -1,12 +1,7 @@
-import { Card } from "antd";
-import { EditOutlined, ReadOutlined } from "@ant-design/icons";
 import { MessageList } from "./DashboardMessageList";
-import { useNavigate } from "react-router-dom";
 import { LeftArrowBar } from "../utils/ArrowBar";
 
 export function DashboardMessage() {
-  let navigate = useNavigate();
-
   return (
     <div className="relative flex flex-col w-full">
       <LeftArrowBar />
